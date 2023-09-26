b4b3d41ede8a7c044416108359b6851546003322
michal <michal.czechowicz7@gmail.com>
2023-09-26 14:09:27 +0200

    added login and profile

--- users/views.py
+++ users/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 class RegisterView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
@@ -16,5 +18,26 @@ class RegisterView(View):
         return render(request, 'users/register.html', {'form': form})
 
 
-class LoginView(TemplateView):
-    template_name = "users/login.html"
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            print('form valid')
+            login(request, form.get_user())
+            return render(request, 'task_manager/main_page.html')
+        return render(request, 'users/login.html', {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('users:login')
+
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'users/profile.html')
