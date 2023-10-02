dc02c7fcc3dda620d04dc7629afbf39de6561f81
michal <michal.czechowicz7@gmail.com>
2023-10-02 20:26:28 +0200

    Editing task

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ app_name = "tasks"
 urlpatterns = [
     path('add/', views.AddTaskView.as_view(), name="add-task"),
     path('<int:pk>/', views.TaskDetailView.as_view(), name="task-view"),
+    path('edit/<int:pk>', views.UpdateTaskView.as_view(), name="update-task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, View, ListView, DetailView, CreateView
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime, timedelta
 from .forms import AddTaskForm
 from .models import Tasks
 
 
-# class AddTaskView(View):
-#     template_name = "tasks/add_task.html"
-#     default_date = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
-#
-#     def get(self, request):
-#         default_date = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
-#         return render(request, self.template_name,
-#                       {'default_date': self.default_date})
-#
-#     def post(self, request):
-#         form = AddTaskForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('task_manager:home-page')
-#         return render(request, self.template_name, {'default_date': self.default_date})
-
-class AddTaskView(CreateView):
+class AddTaskView(LoginRequiredMixin, CreateView):
     template_name = "tasks/add_task.html"
     model = Tasks
     fields = ["title", "deadline", "description", "importance"]
@@ -36,6 +21,14 @@ class AddTaskView(CreateView):
         return super().form_valid(form)
 
 
+class UpdateTaskView(AddTaskView, UpdateView):
+    pass
+
+
 class TaskDetailView(DetailView):
     template_name = "tasks/single_task.html"
     model = Tasks
+
+
+class DeleteTaskView(DeleteView):
+    template_name = ""
