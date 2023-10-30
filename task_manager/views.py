872b9d9dc766367c86b94c78d17dfbd472093dc4
michal <michal.czechowicz7@gmail.com>
2023-10-30 21:03:39 +0100

    Fixed sorting by importance

--- task_manager/views.py
+++ task_manager/views.py
@@ -11,15 +11,38 @@ class TasksListView(ListView):
     paginate_by = 5
     ordering = "deadline"
 
+    IMPORTANCE_MAPPING = {
+        "high": 3,
+        "medium": 2,
+        "low": 1,
+    }
+
+    def importance_sort_key(self, task, order=None):
+        if self.request.session.get('ordering') == "-importance":
+            return -self.IMPORTANCE_MAPPING.get(task.importance, 0)
+        return self.IMPORTANCE_MAPPING.get(task.importance, 0)
+
     def get_queryset(self):
-        return Tasks.objects.filter(author=self.request.user.pk).order_by(self.ordering)
+        ordering = self.request.session.get('ordering', "deadline")
+        queryset = Tasks.objects.filter(author=self.request.user.pk)
+
+        if ordering == "importance" or ordering == "-importance":
+            queryset = sorted(queryset, key=self.importance_sort_key)
+        else:
+            queryset = queryset.order_by(ordering)
+
+        return queryset
+
 
     def get(self, request, *args, **kwargs):
-        if request.GET.get("sort"):
-            self.ordering = request.GET.get("sort")
-
+        current_ordering = self.request.GET.get('sort')
+        if current_ordering:
+            if current_ordering == self.request.session.get("ordering"):
+                self.request.session["ordering"] = '-' + current_ordering
+            else:
+                self.request.session["ordering"] = current_ordering
         self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
+        # allow_empty = self.get_allow_empty()
         context = self.get_context_data()
         return self.render_to_response(context)
 
